47260ff1d77ec25ccf819d34a0e0bb350bbd959e
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-20 23:19:27 +0000

    tried to optimize the bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, swaps, temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		swaps = 0;
 		for (j = 0; j < (size - 1 - i); j++)
